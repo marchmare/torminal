12122f28b11e0daa41d4a380483f9ab0aed88825
Marcel Nowicki <marcel.nowicki94@gmail.com>
2026-07-16 00:26:24 +0200

    Add TUI loading screen

---
+++ src/torminal/tui/loadingscreen.py
@@ -0,0 +1,52 @@
+from textual.app import ComposeResult
+from textual.widgets import Label, ProgressBar
+from textual.screen import ModalScreen
+from textual.containers import Container
+from textual import work
+
+from torminal.gtfs.static import GTFSStaticLoader, GTFSStaticFeed
+from torminal.gtfs.static import ProgressEvent
+
+from asyncio import sleep
+
+LOGO = """░▀█▀░█▀█░█▀▄░█▄█░▀█▀░█▀█░█▀█░█░░░
+░░█░░█░█░█▀▄░█░█░░█░░█░█░█▀█░█░░░
+░░▀░░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀▀▀░"""
+
+
+class LoadingScreen(ModalScreen):
+    def compose(self) -> ComposeResult:
+        with Container(classes="box"):
+            yield Label(LOGO)
+            yield Label("", id="loading_message")
+            yield ProgressBar(show_eta=False, show_percentage=False)
+
+    def on_mount(self) -> None:
+        self.load_data()
+
+    @work(thread=True)
+    def load_data(self) -> None:
+        """Main loader worker, initializes loader and progress bar, then loads GTFS static data."""
+        loader = GTFSStaticLoader(self.update_progress)
+        self.bar.update(total=loader.total)
+        data = loader.load()
+        self.app.call_from_thread(self._loading_finished, data)
+
+    async def _loading_finished(self, data: GTFSStaticFeed) -> None:
+        """Loading finished callback."""
+        await sleep(0.25)
+        self.app.loaded_data = data
+        self.dismiss(data)
+
+    def update_progress(self, progress: ProgressEvent) -> None:
+        """Update progress bar and loading message label state."""
+        self.loading_message.update(progress.message)
+        self.bar.animate("progress", progress.current, duration=0.3)
+
+    @property
+    def bar(self) -> ProgressBar:
+        return self.query_one(ProgressBar)
+
+    @property
+    def loading_message(self) -> Label:
+        return self.query_one("#loading_message", Label)
