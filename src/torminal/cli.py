9d7651d80ece3cceb27009edf0d18089206d5bda
Patryk Kościk <koscikpatryk@gmail.com>
2026-07-15 20:09:54 +0200

    Remove dead TUI import and ignore GTFS artifacts (#1)

--- src/torminal/cli.py
+++ src/torminal/cli.py
@@ -1,13 +1,9 @@
 from torminal.gtfs.static import GTFSStaticLoader
 from torminal.query import Monitor, Query
-from torminal.tui.main import WelcomeApp
 
 
 def app() -> None:
     print("🚋 TORminal")
-    # app = WelcomeApp()
-    # app.run()
-
     loader = GTFSStaticLoader(print_update)
     lookup = loader.load()
     query = Query("NARA71", 10, 60)
