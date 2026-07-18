27f79524d4ccb55313bc9525000c4b493e0221c5
Marcel Nowicki <marcel.nowicki94@gmail.com>
2026-07-18 15:51:23 +0200

    Add httpx to dependencies, rewrite static and requests modules

--- src/torminal/cli.py
+++ src/torminal/cli.py
@@ -1,40 +1,46 @@
 from collections import defaultdict
+import asyncio
 
 from torminal.gtfs.static import GTFSStaticLoader
 from torminal.query import Monitor, Query
 from torminal.gtfs.realtime import fetch_gtfs_rt_feed, fetch_peka_vm_feed
-from datetime import datetime
+from torminal.requests import HTTPXCLIENT
 
 
 def app() -> None:
-    print("🚋 TORminal")
-    loader = GTFSStaticLoader(print_update)
-    dataset = loader.load()
-    print(dataset.feed_info)  # print feed_info to see if dataset applies for today's date
-    query = Query("NARA71", 10)
-    monitor = Monitor(dataset, 60)
+    asyncio.run(main())
 
-    matches = monitor.resolve_query(query)
 
-    # group by stops:
-    matches_grouped = defaultdict(list)
-    for match in matches:
-        matches_grouped[match.stop.code].append(match)
+async def main() -> None:
+    async with HTTPXCLIENT:
+        print("🚋 TORminal")
+        loader = GTFSStaticLoader(print_update)
+        dataset = await loader.load()
+        print(dataset.feed_info)  # print feed_info to see if dataset applies for today's date
+        query = Query("NARA71", 10)
+        monitor = Monitor(dataset, 60)
 
-    # poll:
-    for stop, stop_matches in matches_grouped.items():
-        rt_msg = fetch_peka_vm_feed(monitor.dataset.stops.get(stop))
+        matches = monitor.resolve_query(query)
 
-        for match in stop_matches:
-            rt_gtfs = fetch_gtfs_rt_feed()
+        # group by stops:
+        matches_grouped = defaultdict(list)
+        for match in matches:
+            matches_grouped[match.stop.code].append(match)
 
-            rt_tu, rt_vp = (
-                rt_gtfs.trip_updates.get(match.trip.id, None),
-                rt_gtfs.vehicle_positions.get(match.trip.id, None),
-            )
+        # poll:
+        rt_gtfs = await fetch_gtfs_rt_feed()
 
-            result = monitor.poll(match, rt_tu, rt_vp, rt_msg)
-            print(result)
+        for stop, stop_matches in matches_grouped.items():
+            rt_msg = await fetch_peka_vm_feed(monitor.dataset.stops.get(stop))
+
+            for match in stop_matches:
+                rt_tu, rt_vp = (
+                    rt_gtfs.trip_updates.get(match.trip.id, None),
+                    rt_gtfs.vehicle_positions.get(match.trip.id, None),
+                )
+
+                result = monitor.poll(match, rt_tu, rt_vp, rt_msg)
+                print(result)
 
 
 def print_update(progress):
--- src/torminal/requests.py
+++ src/torminal/requests.py
@@ -1,9 +1,10 @@
 """Module for accessing GTFS files published by ZTM and handling other HTTP requests."""
 
 import requests
+import httpx
 import csv
 import json
-from typing import Any, Generator, Optional
+from typing import Any, Generator, AsyncGenerator, Optional
 from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
 from google.transit import gtfs_realtime_pb2
 from google.transit.gtfs_realtime_pb2 import FeedEntity
@@ -31,56 +32,65 @@ CONFIG_DIR = Path(user_config_dir("TORminal", "marchmare"))
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
 CONFIG_DIR.mkdir(parents=True, exist_ok=True)
 
-HEADERS = {
+headers = {
     "Accept": "application/octet-stream",
     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
 }
+timeout = httpx.Timeout(connect=5, read=30, write=5, pool=5)
+
+HTTPXCLIENT = httpx.AsyncClient(headers=headers, timeout=timeout)
 
 
-def download_file(output_file: str, url: str, params: Optional[str] = "") -> None:
+async def download_file(output_file: str, url: str, params: Optional[str] = "") -> None:
     """Download file helper function."""
     _url = f"{url}{params}"
-    response = requests.get(_url, headers=HEADERS, stream=True)
-    response.raise_for_status()
-    with open(f"{CACHE_DIR}/{output_file}", "wb") as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
+
+    async with HTTPXCLIENT.stream("GET", _url, headers=headers) as response:
+        response.raise_for_status()
+
+        with open(f"{CACHE_DIR}/{output_file}", "wb") as f:
+            async for chunk in response.aiter_bytes(8192):
+                f.write(chunk)
 
 
-def fetch_protobuf(url: str) -> RepeatedCompositeFieldContainer[FeedEntity]:
+async def fetch_protobuf(url: str) -> RepeatedCompositeFieldContainer[FeedEntity]:
     """Fetch GTFS Protobuf feed from specified URL."""
 
     feed = gtfs_realtime_pb2.FeedMessage()
-    response = requests.get(url)
+
+    response = await HTTPXCLIENT.get(url)
     response.raise_for_status()
+
     feed.ParseFromString(response.content)
     return feed.entity
 
 
-def fetch_form_post(url: str, method: str, params: dict[str, Any]) -> Any:
+async def fetch_form_post(url: str, method: str, params: dict[str, Any]) -> Any:
     """Fetch response from URL using form POST with JSON blobs inside parameters."""
 
     _params = json.dumps(params)
     payload = {"method": method, "p0": _params}
 
-    response = requests.post(
+    response = await HTTPXCLIENT.post(
         url,
         params={"ts": int(time() * 1000)},
         data=payload,
-        headers=HEADERS,
+        headers=headers,
     )
     response.raise_for_status()
+
     return response.json()
 
 
-def get_gtfs_archive_list() -> list[GTFSArchive]:
+async def _get_gtfs_archive_list() -> list[GTFSArchive]:
     """
     Scrape list of available GTFS archives from https://www.ztm.poznan.pl/otwarte-dane/gtfsfiles/
     The most recent archives are on top.
     """
 
-    response = requests.get(GTFS_SITE_URL)
+    response = await HTTPXCLIENT.get(GTFS_SITE_URL)
     response.raise_for_status()
+
     soup = BeautifulSoup(response.text, "html.parser")
     archives: list[GTFSArchive] = []
 
@@ -97,12 +107,12 @@ def get_gtfs_archive_list() -> list[GTFSArchive]:
     return archives
 
 
-def resolve_current_gtfs_archive() -> GTFSArchive | None:
+async def resolve_current_gtfs_archive() -> GTFSArchive | None:
     """
     Resolve most recent and currently applicable GTFS archive.
     ZTM publishes GTFS archives ahead of time and with overlapping dates.
     """
-    archives = get_gtfs_archive_list()
+    archives = await _get_gtfs_archive_list()
     today = datetime.today().date()
 
     for archive in archives:
@@ -111,14 +121,10 @@ def resolve_current_gtfs_archive() -> GTFSArchive | None:
     return None
 
 
-@contextmanager
-def open_gtfs_zip() -> Generator[ZipFile, Any, None]:
-    """
-    Download and read GTFS zip archive with trips, stop times, stops, shapes, routes, feed_info, calendar dates, calendar and agency data.
+async def fetch_gtfs_zip() -> None:
+    """Download GTFS zip archive with trips, stop times, stops, shapes, routes, feed_info, calendar dates, calendar and agency data."""
 
-        Documentation: https://www.ztm.poznan.pl/wp-content/uploads/2024/07/Specyfikacja-GTFS-04.02.2022.pdf
-    """
-    current_archive = resolve_current_gtfs_archive()
+    current_archive = await resolve_current_gtfs_archive()
     if not current_archive:
         raise RuntimeError("No available GTFS archive.")
 
@@ -129,21 +135,35 @@ def open_gtfs_zip() -> Generator[ZipFile, Any, None]:
     # potential way to workaround this: download and load two datasets, swap if one doesn't yield any results,
     # ideally with some early check during loading
     #
-    download_file(output_file=GTFS_FILE_NAME, url=GTFS_FILE_URL, params=f"/?file={current_archive.filename}")
-    # download_file(output_file=GTFS_FILE_NAME, url=GTFS_FILE_URL)
+    await download_file(output_file=GTFS_FILE_NAME, url=GTFS_FILE_URL, params=f"/?file={current_archive.filename}")
+    # await download_file(output_file=GTFS_FILE_NAME, url=GTFS_FILE_URL)
+
+
+@contextmanager
+def open_gtfs_zip() -> Generator[ZipFile, Any, None]:
+    """
+    Read GTFS zip archive with trips, stop times, stops, shapes, routes, feed_info, calendar dates, calendar and agency data.
+
+        Documentation: https://www.ztm.poznan.pl/wp-content/uploads/2024/07/Specyfikacja-GTFS-04.02.2022.pdf
+    """
 
     with ZipFile(f"{CACHE_DIR}/{GTFS_FILE_NAME}") as z:
         yield z
 
 
+async def fetch_vehicle_dictionary() -> None:
+    """Download vehicle_dictionary.csv with vehicle properties."""
+
+    await download_file(output_file=VEHICLE_DICTIONARY_NAME, url=VEHICLE_DICTIONARY_URL)
+
+
 @contextmanager
 def open_vehicle_dictionary() -> Generator[csv.DictReader, Any, None]:
     """
-    Download and read vehicle_dictionary.csv with vehicle properties.
+    Read vehicle_dictionary.csv with vehicle properties.
 
         Documentation: https://www.ztm.poznan.pl/wp-content/uploads/2024/07/slownik-pojazdow-opis.pdf
     """
-    download_file(output_file=VEHICLE_DICTIONARY_NAME, url=VEHICLE_DICTIONARY_URL)
 
     with open(f"{CACHE_DIR}/{VEHICLE_DICTIONARY_NAME}", "r") as file:
         yield csv.DictReader(file)
