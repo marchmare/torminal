0e628ab96bccbf2c0f4ad7a97a01b52c968bebb6
Marcel Nowicki <marcel.nowicki94@gmail.com>
2026-07-18 16:51:04 +0200

    Rewrite realtime module

--- src/torminal/cli.py
+++ src/torminal/cli.py
@@ -7,10 +7,6 @@ from torminal.gtfs.realtime import fetch_gtfs_rt_feed, fetch_peka_vm_feed
 from torminal.requests import HTTPXCLIENT
 
 
-def app() -> None:
-    asyncio.run(main())
-
-
 async def main() -> None:
     async with HTTPXCLIENT:
         print("🚋 TORminal")
@@ -27,20 +23,25 @@ async def main() -> None:
         for match in matches:
             matches_grouped[match.stop.code].append(match)
 
+        # get peka feeds in parallel
+        peka_tasks = {stop: fetch_peka_vm_feed(monitor.dataset.stops.get(stop)) for stop in matches_grouped}
+        peka_results = await asyncio.gather(*peka_tasks.values())
+        peka_feeds = dict(zip(peka_tasks.keys(), peka_results))
+
         # poll:
         rt_gtfs = await fetch_gtfs_rt_feed()
-
         for stop, stop_matches in matches_grouped.items():
-            rt_msg = await fetch_peka_vm_feed(monitor.dataset.stops.get(stop))
-
+            rt_msg = peka_feeds[stop]
             for match in stop_matches:
-                rt_tu, rt_vp = (
-                    rt_gtfs.trip_updates.get(match.trip.id, None),
-                    rt_gtfs.vehicle_positions.get(match.trip.id, None),
-                )
+                rt_tu = rt_gtfs.trip_updates.get(match.trip.id, None)
+                rt_vp = rt_gtfs.vehicle_positions.get(match.trip.id, None)
 
                 result = monitor.poll(match, rt_tu, rt_vp, rt_msg)
                 print(result)
+
+
+def app() -> None:
+    asyncio.run(main())
 
 
 def print_update(progress):
