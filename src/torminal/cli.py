e3d1a2ec6f67d30ef91af8e3ac6ee268d2a23fe5
Marcel Nowicki <marcel.nowicki94@gmail.com>
2026-07-28 21:15:16 +0200

    Add cli argument for adding queries to config

--- src/torminal/cli.py
+++ src/torminal/cli.py
@@ -1,3 +1,12 @@
+def add(args) -> None:
+    from torminal.config import config
+
+    config.add_query(args.add)
+    print("Updated config:")
+    print(f"\t ⬩ stop code: {args.add[0]}")
+    print(f"\t ⬩ route ID: {args.add[1]}")
+
+
 def app() -> None:
     from torminal.tui.app import TORminal
     import argparse
@@ -9,6 +18,18 @@ def app() -> None:
         required=False,
         action="store_true",
     )
+    parser.add_argument(
+        "-a",
+        "--add",
+        nargs=2,
+        metavar=("[stop_code]", "[route_id]"),
+        help="Add new query to config (headless)",
+        required=False,
+    )
     args = parser.parse_args()
+
+    if args.add:
+        add(args)
+        exit()
 
     TORminal(headless=args.headless).run()
